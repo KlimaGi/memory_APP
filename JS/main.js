f07b8cde3b1d30090ab08a23ba06bba9de702082
Giedre Klimaite <giedre.klima@gmail.com>
2020-11-16 22:31:14 +0200

    fixing and breaking code

--- JS/main.js
+++ JS/main.js
@@ -1,8 +1,9 @@
 //const userLearningDate = new Date();
 const learnMilestoneEndTime = new Date();
 
+
 var year = learnMilestoneEndTime.getFullYear();
-var month = learnMilestoneEndTime.getMonth() + 1; // 0 is January,
+var month = learnMilestoneEndTime.getMonth() + 1; // 0+1 is January,
 var day = learnMilestoneEndTime.getDate();
 var hours = learnMilestoneEndTime.getHours();
 var min = learnMilestoneEndTime.getMinutes();
@@ -15,12 +16,13 @@ var day5 = month + 6;
 
 var learnDay = [min, hours, day, month, year];
 var now = [day1, hours, day, month, year];
+
+function renderDay(){
 //time frame
 var dateAfterCorrection = [];
 
 function checkMinAndHour(now) {
   if (now[0] < 60) {
-    now[0] = now[0]; //work with min
     dateAfterCorrection.push(now[0]);
     dateAfterCorrection.push(now[1]);
   } else if (now[0] == 60) {
@@ -52,26 +54,30 @@ function checkHourAndDay(now, dateAfterCorrection) {
   }
   return dateAfterCorrection;
 }
-
-var maxDayOfMonth = function howManyDaysHasMonth(arr) {
-  if (
-    arr[3] === 1 ||
-    arr[3] === 3 ||
-    arr[3] === 5 ||
-    arr[3] === 7 ||
-    arr[3] === 8 ||
-    arr[3] === 10 ||
-    arr[3] === 12
+  
+var maxDayOfMonth = function howManyDaysHasMonth(now) {
+   if (
+    now[3] === 1 ||
+    now[3] === 3 ||
+    now[3] === 5 ||
+    now[3] === 7 ||
+    now[3] === 8 ||
+    now[3] === 10 ||
+    now[3] === 12
   ) {
     maxDayOfMonth = 31;
-  } else if (arr[3] === 4 || arr[3] === 6 || arr[3] === 9 || arr[3] === 11) {
+  } else if (now[3] === 4 || now[3] === 6 || now[3] === 9 || now[3] === 11) {
     maxDayOfMonth = 30;
-  } else if (arr[3] === 2) {
-    if ((arr[4] % 100 === 0) ? (arr[4] % 400 === 0):(arr[4]%4 ===0)) {
+  } else if (now[3] === 2) {
+    if ((now[4] % 100 === 0) ? (now[4] % 400 === 0) : (now[4] % 4 ===0)) {
       maxDayOfMonth = 28;
     } else {
       maxDayOfMonth = 29;
     }
+  } else if (now[3]>12){
+    let month = now[3]-12;
+  now[3]=month;
+    maxDayOfMonth(now[3]);
   }
   return maxDayOfMonth;
 };
@@ -88,30 +94,48 @@ function checkDayAndMonth(dateAfterCorrection, now) {
   return dateAfterCorrection;
 }
 
-function checkMonthAndYear(dateAfterCorrection, now) {
-  if (dateAfterCorrection[3] <= 12) {
+function checkMonthAndYear(now, dateAfterCorrection) {
+  if (now[3] <= 12) {
     dateAfterCorrection.push(now[4]);
-  } else if (dateAfterCorrection[3] > 12) {
-    let month = dateAfterCorrection[3] - 12;
+  } else if (now[3] > 12) {
+    let month = now[3] - 12;
     dateAfterCorrection[3] = month;
     let year = now[4] + 1;
     dateAfterCorrection.push(year);
+    maxDayOfMonth(month);
   }
   return dateAfterCorrection;
 }
 
-console.log(learnMilestoneEndTime);
-console.log(learnDay);
-console.log(dateAfterCorrection);
-console.log(checkMinAndHour(now));
-console.log(checkHourAndDay(now, dateAfterCorrection));
-console.log("max day of this month "+ maxDayOfMonth(now));
-console.log(checkDayAndMonth(dateAfterCorrection, now));
-console.log(checkMonthAndYear(dateAfterCorrection, now));
+// console.log(learnMilestoneEndTime);
+// console.log(learnDay);
+// console.log(dateAfterCorrection);
+// console.log(checkMinAndHour(now));
+// console.log(checkHourAndDay(now, dateAfterCorrection));
+// //console.log(fixMonth(dateAfterCorrection));
+// console.log(maxDayOfMonth);
+// console.log(checkDayAndMonth(dateAfterCorrection, now));
+// console.log(checkMonthAndYear(now, dateAfterCorrection));
 
-document.getElementById("now").innerHTML = learnDay;
+return dateAfterCorrection;
+}
+renderDay();
+let firstDate = renderDay(now);
+let secondDate = renderDay(countSecondDate (firstDate));
 
+function countSecondDate(firstDate){
+const [,d2,,,]= firstDate;
+const changedD2 = d2+1;
+firstDate = [,changedD2,,,];
+return firstDate; 
+}
 
+// document.getElementById("today").innerHTML = learnDay;
+// document.getElementById("day1").innerHTML = renderDay(now);
+// document.getElementById("day2").innerHTML = countSecondDate(firstDate);
+
+console.log(firstDate);
+console.log(countSecondDate(firstDate));
 // tikrinu arr su day1, day2, day3 kintamaisiais juose
 
 //surašau kodą ir tik tada išskaidau į atskirus failus kodo elementus.
