70f5f37148e404583e0405871dfd0afed2cb4534
Giedre Klimaite <giedre.klima@gmail.com>
2020-11-18 22:34:20 +0200

    changes after breakpoint

--- JS/main.js
+++ JS/main.js
@@ -1,144 +1,34 @@
-//const userLearningDate = new Date();
-const learnMilestoneEndTime = new Date();
+function countDays(days){
+var someDate = new Date();
+var numberOfDaysToAdd = days;
+someDate.setDate(someDate.getDate() + numberOfDaysToAdd); 
 
+var dd = someDate.getDate();
+var mm = someDate.getMonth() + 1;
+var y = someDate.getFullYear();
 
-var year = learnMilestoneEndTime.getFullYear();
-var month = learnMilestoneEndTime.getMonth() + 1; // 0+1 is January,
-var day = learnMilestoneEndTime.getDate();
-var hours = learnMilestoneEndTime.getHours();
-var min = learnMilestoneEndTime.getMinutes();
-
-var day1 = min + 10; 
-var day2 = day + 1; 
-var day3 = day + 7;
-var day4 = month + 1;
-var day5 = month + 6;
-
-var learnDay = [min, hours, day, month, year];
-var now = [day1, hours, day, month, year];
-
-function renderDay(){
-//time frame
-var dateAfterCorrection = [];
-
-function checkMinAndHour(now) {
-  if (now[0] < 60) {
-    dateAfterCorrection.push(now[0]);
-    dateAfterCorrection.push(now[1]);
-  } else if (now[0] == 60) {
-    now[0] = 0; //work with min
-    now[1] += 1;
-    dateAfterCorrection.push(now[0]);
-    dateAfterCorrection.push(now[1]);
-  } else if (now[0] > 60) {
-    now[0] = now[0] - 60; // work with min and hour
-    now[1] = now[1] + 1;
-    dateAfterCorrection.push(now[0]);
-    dateAfterCorrection.push(now[1]);
-  }
-  return dateAfterCorrection;
+var someFormattedDate = dd + '/'+ mm + '/'+ y;
+return someFormattedDate;
 }
+countDays();
 
-function checkHourAndDay(now, dateAfterCorrection) {
-  if (now[1] == 24) {
-    now[1] = 0;
-    let day = now[2] + 1; // work with hour and day
-    dateAfterCorrection.push(day);
-  } else if (now[1] > 24) {
-    let hour = now[1] - 24;
-    let day = now[2] + 1;
-    dateAfterCorrection.push(hour);
-    dateAfterCorrection.push(day);
-  } else if (now[1] <= 23) {
-    dateAfterCorrection.push(now[2]);
-  }
-  return dateAfterCorrection;
-}
-  
-var maxDayOfMonth = function howManyDaysHasMonth(now) {
-   if (
-    now[3] === 1 ||
-    now[3] === 3 ||
-    now[3] === 5 ||
-    now[3] === 7 ||
-    now[3] === 8 ||
-    now[3] === 10 ||
-    now[3] === 12
-  ) {
-    maxDayOfMonth = 31;
-  } else if (now[3] === 4 || now[3] === 6 || now[3] === 9 || now[3] === 11) {
-    maxDayOfMonth = 30;
-  } else if (now[3] === 2) {
-    if ((now[4] % 100 === 0) ? (now[4] % 400 === 0) : (now[4] % 4 ===0)) {
-      maxDayOfMonth = 28;
-    } else {
-      maxDayOfMonth = 29;
-    }
-  } else if (now[3]>12){
-    let month = now[3]-12;
-  now[3]=month;
-    maxDayOfMonth(now[3]);
-  }
-  return maxDayOfMonth;
-};
+var learnDay = countDays(0);
+//var day1 = min + 10;
+//  --> sukurti alert priminima pakartojimui informacijos, naudoti countdown-timer logika
+var day2 = countDays(1); 
+var day3 = countDays(7);
+var day4 = countDays(30);
+var day5 = countDays(182);
 
-function checkDayAndMonth(dateAfterCorrection, now) {
-  if (dateAfterCorrection[2] <= maxDayOfMonth) {
-    dateAfterCorrection.push(now[3]); //arr[3] is month
-  } else if (dateAfterCorrection[2] > maxDayOfMonth) {
-    let day = dateAfterCorrection[2] - maxDayOfMonth;
-    dateAfterCorrection[2] = day;
-    let month = now[3] + 1;
-    dateAfterCorrection.push(month);
-  }
-  return dateAfterCorrection;
-}
+document.getElementById("today").innerHTML = learnDay;
+//document.getElementById("day1").innerHTML = someFormattedDate;
+document.getElementById("day2").innerHTML = day2;
+document.getElementById("day3").innerHTML = day3;
+document.getElementById("day4").innerHTML = day4;
+document.getElementById("day5").innerHTML = day5;
 
-function checkMonthAndYear(now, dateAfterCorrection) {
-  if (now[3] <= 12) {
-    dateAfterCorrection.push(now[4]);
-  } else if (now[3] > 12) {
-    let month = now[3] - 12;
-    dateAfterCorrection[3] = month;
-    let year = now[4] + 1;
-    dateAfterCorrection.push(year);
-    maxDayOfMonth(month);
-  }
-  return dateAfterCorrection;
-}
-
-// console.log(learnMilestoneEndTime);
 // console.log(learnDay);
-// console.log(dateAfterCorrection);
-// console.log(checkMinAndHour(now));
-// console.log(checkHourAndDay(now, dateAfterCorrection));
-// //console.log(fixMonth(dateAfterCorrection));
-// console.log(maxDayOfMonth);
-// console.log(checkDayAndMonth(dateAfterCorrection, now));
-// console.log(checkMonthAndYear(now, dateAfterCorrection));
-
-return dateAfterCorrection;
-}
-renderDay();
-let firstDate = renderDay(now);
-let secondDate = renderDay(countSecondDate (firstDate));
-
-function countSecondDate(firstDate){
-const [,d2,,,]= firstDate;
-const changedD2 = d2+1;
-firstDate = [,changedD2,,,];
-return firstDate; 
-}
-
-// document.getElementById("today").innerHTML = learnDay;
-// document.getElementById("day1").innerHTML = renderDay(now);
-// document.getElementById("day2").innerHTML = countSecondDate(firstDate);
-
-console.log(firstDate);
-console.log(countSecondDate(firstDate));
-// tikrinu arr su day1, day2, day3 kintamaisiais juose
-
-//surašau kodą ir tik tada išskaidau į atskirus failus kodo elementus.
-//Geras kodo rašymas, kai kodo elementas telpa į ekraną
-//svarbu funkcionalumas
-// pop up priminimas arba kitas priminimo formatas, veliau skaitau apie priminimo formatus, pvz. duolingo priminimai
+// console.log(day2);
+// console.log(day3);
+// console.log(day4);
+// console.log(day5);
